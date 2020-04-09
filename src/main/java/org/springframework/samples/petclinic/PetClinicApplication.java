433b32e018371905889b20cd1625c3b10b681ed1
ShikharGoel04 <48277817+ShikharGoel04@users.noreply.github.com>
2020-04-09 10:53:12 +0530

    Update PetClinicApplication.java

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -28,12 +28,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(proxyBeanMethods = false)
 public class PetClinicApplication extends SpringBootServletInitializer {
 
-    @Override
+ @Override
  protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
          return app.sources(PetClinicApplication.class);
-     }
-	public static void main(String[] args) {
+}
+ public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
-	}
+}
 
 }
